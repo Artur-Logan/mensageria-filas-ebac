e116176a71a89cceea1ef62bd303ca3cc6624e60
Artur Logan <arturlogan1ferreira@gmail.com>
2025-01-10 15:12:20 -0300

    Primeiro commit

---
+++ src/main/java/com/arturlogan/mensageria_filas_ebac/MensageriaFilasEbacApplication.java
@@ -0,0 +1,13 @@
+package com.arturlogan.mensageria_filas_ebac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensageriaFilasEbacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MensageriaFilasEbacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arturlogan/mensageria_filas_ebac/MensageriaFilasEbacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturlogan.mensageria_filas_ebac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensageriaFilasEbacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
